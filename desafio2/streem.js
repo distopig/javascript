928ae18226f5962db6f529c42f7ee6403b4255bf
onailo <107935054+distopig@users.noreply.github.com>
2022-10-18 16:58:48 -0300

    alguma coisa

--- desafio2/streem.js
+++ desafio2/streem.js
@@ -1,1 +1,13 @@
-alert('hello work')
+function verificar(){
+    var data = new date()
+    var ano = data.getfullyear()
+    var fano = document.getElementsByid('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        alert('verifique os dados')
+    } else {
+        var fsex = document.getElementsByName('radio')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `idade de ${idade}`
+    }
+}
